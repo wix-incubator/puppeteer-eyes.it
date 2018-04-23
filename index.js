2f88759b0417a9f750216ae6bf036c374faf7f68
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-04-23 21:48:03 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const uuid = require('uuid');
+const {Eyes} = require('eyes.images');
+
+const appName = require(path.join(process.cwd(), 'package.json')).name;
+const eyes = new Eyes();
+
+function handleError(err, done) {
+  fail(err);
+  done();
+}
+
+function isPassedWindowSizeArgument(argumentsObj) {
+  return typeof argumentsObj[2] === 'object';
+}
+
+function eyesWithout(fn) {
+  return function() {
+    if (isPassedWindowSizeArgument(arguments)) {
+      delete arguments[2];
+    }
+    return fn.apply(this, arguments);
+  };
+}
+
+function eyesWith(fn) {
+  return function() {
+    let windowSize = eyes.defaultWindowSize;
+    if (isPassedWindowSizeArgument(arguments)) {
+      windowSize = arguments[2];
+      delete arguments[2];
+    }
+    const spec = fn.apply(this, arguments);
+    const hooked = spec.beforeAndAfterFns;
+    spec.beforeAndAfterFns = function() {
+      const result = hooked.apply(this, arguments);
+      result.befores.unshift({
+        fn(done) {
+          eyes.open(appName, `eyes.it ${spec.getFullName()}`, windowSize).then(done);
+        },
+        timeout: () => 30000,
+      });
+      result.afters.unshift({
+        async fn(done) {
+          try {
+            const img = await global.page.screenshot();
+            await eyes.checkImage(img, spec.getFullName());
+            await eyes.close();
+            done();
+          } catch (err) {
+            handleError(err, done);
+          }
+        },
+        timeout: () => 30000,
+      });
+      return result;
+    };
+    return spec;
+  };
+}
+
+function getBatchUUID() {
+  return process.env.EYES_BATCH_UUID;
+}
+
+function setOnceBatchUUID(uuid) {
+  if (!getBatchUUID()) {
+    process.env.EYES_BATCH_UUID = uuid;
+  }
+}
+
+function _init() {
+  if (process.env.IS_BUILD_AGENT) {
+    process.env.EYES_BATCH_UUID = process.env.BUILD_NUMBER;
+  }
+
+  setOnceBatchUUID(uuid.v4());
+
+  if (process.env.EYES_API_KEY) {
+    eyes.setApiKey(process.env.EYES_API_KEY);
+    eyes.it = eyesWith(it);
+    eyes.fit = eyesWith(fit);
+    eyes.test = eyesWith(test);
+  } else {
+    eyes.it = eyesWithout(it);
+    eyes.fit = eyesWithout(fit);
+    eyes.test = eyesWithout(test);
+  }
+
+  eyes.defaultWindowSize = null;
+  eyes.setBatch(appName, getBatchUUID());
+}
+
+_init();
+
+module.exports = eyes;
---
+++ test/test.spec.js
@@ -0,0 +1,13 @@
+const eyes = require('../index');
+
+[
+  {fnName: 'eyes.it', fn: eyes.it},
+  {fnName: 'it', fn: it},
+  {fnName: 'eyes.test', fn: eyes.test},
+  {fnName: 'test', fn: test}
+].forEach(type => {
+  type.fn(`should work for ${type.fnName}`, async () => {
+    await global.page.goto('http://localhost:3000');
+    expect(await global.page.$eval('div', el => el.innerText)).toEqual('Hi there!');
+  });
+});
