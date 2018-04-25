4e816a4d24de6b5b24954b8c232f22b7b6a26265
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-04-25 10:55:50 +0300

    add args for ci

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  launch: {
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage', // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#tips
+    ],
+  },
   server: {
     command: 'node test/server.js',
     port: 3000,
