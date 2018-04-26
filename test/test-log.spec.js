dca9013a4adf167133bb2555e0433d722899894e
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-04-26 21:29:48 +0300

    refactor

--- test/test-log.spec.js
+++ test/test-log.spec.js
@@ -5,6 +5,8 @@ describe('Log', () => {
   it('should log after eyes success', async () => {
     const {spawn} = require('child_process');
     let _data;
+    let fullfill;
+    const p = new Promise(resolve => (fullfill = resolve));
 
     const res = spawn('node', [
       path.join(__dirname, '..', 'node_modules', 'jest', 'bin', 'jest.js'),
@@ -13,14 +15,8 @@ describe('Log', () => {
       path.join(__dirname, '..', 'index.js'),
     ]);
     res.stdout.on('data', data => {
-      console.log(data.toString('utf8'));
       _data += data.toString('utf8');
     });
-    res.stderr.on('data', data => {
-      _data += data.toString('utf8');
-    });
-    let fullfill;
-    const p = new Promise(resolve => (fullfill = resolve));
     res.on('close', () => fullfill());
     await p;
     expect(_data).toContain('eyes comparison succeed');
