ba2e9d7a1bea00f4d5f6c6d766cc0e00c90b698d
Yaniv Efraim <yaniv.efraim@gmail.com>
2018-04-26 22:05:59 +0300

    health: refactor test

--- test/fixtures/test.customSpec.js
+++ test/fixtures/test.customSpec.js
@@ -3,11 +3,18 @@ const puppeteer = require('puppeteer');
 const eyesUrl = process.argv[4];
 const eyes = require(eyesUrl);
 let browser;
-beforeAll(async () => (browser = await puppeteer.launch()));
+beforeAll(async () =>
+  (browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage', // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#tips
+    ],
+  })));
 afterAll(() => browser.close());
 
 eyes.it(`should work`, async () => {
   global.page = await browser.newPage();
-  await global.page.goto('about:blank');
+  await global.page.setContent('<div>Hello World</div>');
   expect(true).toEqual(true);
 });
